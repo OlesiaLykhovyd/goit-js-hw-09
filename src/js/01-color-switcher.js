c823ef21d0cf7364a0515e54bbb67a9f41023bf7
OlesiaLykhovyd <olesyalykhovyd@gmail.com>
2021-12-24 20:53:31 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+startButton.addEventListener('click', addBcgColor);
+stopButton.addEventListener('click', stopBcgColor);
+let colorId = null;
+
+function addBcgColor() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  colorId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopBcgColor() {
+  clearInterval(colorId);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
